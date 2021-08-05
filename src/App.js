0ee766cffeb3cc7bb24778d4232f12c0968908b4
lillapulay <59831508+lillapulay@users.noreply.github.com>
2021-08-05 16:10:31 +0200

    Added missing content for default test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h3>Testing workflow</h3>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>Also, CI/CD is cool.</p>
+      </header>
     </div>
   );
 }
